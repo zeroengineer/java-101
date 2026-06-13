84540060900bbcc2d9ef69eb9b5620dc35dda59e
zeroengineer <iamdevangcv@gmail.com>
2026-06-13 23:57:12 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("i like pizza");
+        System.out.println("it's good");
+        int age = 21;
+        System.out.println(age);
+        double ae = 34.5;
+        System.out.println(ae);
+        System.out.println("i am old" + ae);
+        String name = "devang";
+        System.out.println(name);
+    }
+}
